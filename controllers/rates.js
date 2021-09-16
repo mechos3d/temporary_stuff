8a804d6066f76074f82d08d956698509f4587ae5
Gorgolevskiy Pavel <gorgolevsky@gmail.com>
2021-09-16 10:10:54 +0300

    review comments

--- controllers/rates.js
+++ controllers/rates.js
@@ -15,8 +15,10 @@ module.exports = async (req, res, next) => {
   //the first - get rates from remote. If service is unavaliable - then from db
   const providerConfigFile = path.resolve(__dirname, '..', 'config', 'sources', provider + '.json')
   if(!fs.existsSync(providerConfigFile)) {
+    // review: плохо что здесь статус 200 при 'Provider not found'
     res.status(200).send('Provider not found')
   }
+  // review: чтение одного и того же файла из файловой системы прямо в запросе - неоптимально, его можно прочтитать один раз при инициализации приложения (с учетом, что этот список не меняется постоянно - но в этом случае лучше бы хранить его в БД)
   const providerConfig = JSON.parse(fs.readFileSync(providerConfigFile))
     ,handlersFolder = path.resolve(__dirname, '..', 'lib', 'handlers')
     ,data = await require(path.resolve(handlersFolder, providerConfig.handler+'.js')).call(null, providerConfig, {from, to});
